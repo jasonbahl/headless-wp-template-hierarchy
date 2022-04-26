907cf60bbec0e156480b5b998aad7d7474312eb4
Jason Bahl <jasonbahl@mac.com>
2022-04-26 11:17:46 -0600

    no message

--- next-apollo/pages/[...WordPressNode].js
+++ next-apollo/pages/[...WordPressNode].js
@@ -1,38 +1,38 @@
 import { gql } from '@apollo/client'
 import { wordPressServerSideProps, WordPressNode } from 'wp-next'
 
-const templates = {
-    'singular': {
-        query: gql`{__typename}`,
-        component: props => {
-            return (
-                <>
-                    <h2>This is my custom singular template</h2>
-                    <pre>{JSON.stringify(props, null, 2)}</pre>
-                </>
-            )
-        }
-    }
-}
+// const templates = {
+//     'singular': {
+//         query: gql`{__typename}`,
+//         component: props => {
+//             return (
+//                 <>
+//                     <h2>This is my custom singular template</h2>
+//                     <pre>{JSON.stringify(props, null, 2)}</pre>
+//                 </>
+//             )
+//         }
+//     }
+// }
 
 
 const WordPressNodeTemplate = props => {
-    return <WordPressNode templates={templates} {...props} />
+    return <WordPressNode templates={{}} {...props} />
 }
 
 export default WordPressNodeTemplate
 
-export async function getServerSideProps(context) {
- return await wordPressServerSideProps(context)
+// export async function getServerSideProps(context) {
+//  return await wordPressServerSideProps(context)
+// }
+
+export async function getStaticProps(context) {
+    return await wordPressServerSideProps(context)
 }
 
-// export async function getStaticProps(context) {
-//     return await wordPressServerSideProps(context)
-// }
-
-// export async function getStaticPaths() {
-//     return {
-//         paths: [],
-//         fallback: 'blocking'
-//     }
-// }
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
